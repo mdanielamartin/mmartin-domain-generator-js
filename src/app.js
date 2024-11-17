e26cea32e801987cf424e2dad27897ef456b0fe2
Maria Daniela Martin <m.danielamartin@proton.me>
2024-11-16 20:02:38 -0400

    fix/optimized w/ function

--- src/app.js
+++ src/app.js
@@ -3,31 +3,29 @@ let adjetives = ['great', 'epic', 'creative', 'bold', 'radiant'];
 let nouns = ['car', 'resort', 'flight', 'book', 'dog', 'internet', 'cactus', 'glove'];
 let extensions = ['.com', '.net', '.gov', '.us', '.ve'];
 let count = 0;
+
 for (let pronoun of pronouns) {
   for (let adj of adjetives) {
     for (let noun of nouns) {
       for (let ext of extensions) {
-        let domain = [];
         if (ext.length === 4 && ext.slice(-3) === noun.slice(-3)) {
-          let domainHack = []
           let nounHack = noun.slice(0, noun.length - 3);
-          domainHack.push(pronoun, adj, nounHack, ext)
-          console.log(domainHack.join(""));
-          count += 1;
+          buildDomain(pronoun, adj, nounHack, ext);
         } else if (ext.length === 3 && ext.slice(-2) === noun.slice(-2)) {
-          let domainHack = []
           let nounHack = noun.slice(0, noun.length - 2);
-          domainHack.push(pronoun, adj, nounHack, ext);
-          console.log(domainHack.join(""));
-          count += 1;
+          buildDomain(pronoun, adj, nounHack, ext);
         }
-        domain.push(pronoun, adj, noun, ext);
-        console.log(domain.join(""));
-        count += 1;
+        buildDomain(pronoun, adj, noun, ext);
       }
     }
   }
 }
 
+function buildDomain(pronoun, adjective, noun, extension) {
+  count += 1;
+  let domain = pronoun + adjective + noun + extension;
+  console.log(domain);
+  return
+}
 
 console.log(`Total Number of Domains Created: ${count}`)
